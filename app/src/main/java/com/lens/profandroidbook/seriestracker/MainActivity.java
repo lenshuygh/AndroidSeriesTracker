b450c10333d21b8f7016ac87cf16d154da841b52
lens <lens.huygh@gmail.com>
2019-11-17 20:16:29 +0100

    add SwitchFragment() to MainActivity to replace series fragment with episodes

--- app/src/main/java/com/lens/profandroidbook/seriestracker/MainActivity.java
+++ app/src/main/java/com/lens/profandroidbook/seriestracker/MainActivity.java
@@ -8,32 +8,43 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG_LIST_FRAGMENT = "TAG_LIST_FRAGMENT";
+    private static final String TAG_SERIES_FRAGMENT = "TAG_SERIES_FRAGMENT";
+    private static final String TAG_EPISODES_FRAGMENT = "TAG_EPISODES_FRAGMENT";
 
     SeriesListFragment seriesListFragment;
 
+    EpisodeListFragment episodeListFragment;
+
     SeriesViewModel seriesViewModel;
+
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager = getSupportFragmentManager();
 
         if (savedInstanceState == null) {
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
             seriesListFragment = new SeriesListFragment();
-            fragmentTransaction.add(R.id.main_activity_frame, seriesListFragment, TAG_LIST_FRAGMENT);
+            fragmentTransaction.add(R.id.main_activity_frame, seriesListFragment, TAG_SERIES_FRAGMENT);
             fragmentTransaction.commitNow();
         } else {
-            seriesListFragment = (SeriesListFragment) fragmentManager.findFragmentByTag(TAG_LIST_FRAGMENT);
+            seriesListFragment = (SeriesListFragment) fragmentManager.findFragmentByTag(TAG_SERIES_FRAGMENT);
 
         }
 
     seriesViewModel = ViewModelProviders.of(this).get(SeriesViewModel.class);
 
+    }
+
+    void SwitchFragment(){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_activity_frame,new EpisodeListFragment(),TAG_EPISODES_FRAGMENT);
+        fragmentTransaction.commitNow();
     }
 
 }
